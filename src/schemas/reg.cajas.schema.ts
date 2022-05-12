28e8b1fcc2c68c937c5725c5bda675e0f6ca425e
Chispa <camilomonterom@gmail.com>
2022-05-12 17:33:07 -0400

    asdas

---
+++ src/schemas/reg.cajas.schema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export const regEstadoUserSchema = z.object({
+    body: z.object({
+        TotalCaja: z.number().nonnegative('No puede ser negativo').min(50000, 'No contiene la suficiente cantidad de numeros'),
+        TotalTarjeta: z.number().nonnegative('No puede ser negativo').min(1, 'No contiene la suficiente cantidad de numeros'),
+        TotalTransf: z.number().nonnegative('No puede ser negativo'),
+        SiguienteDia: z.number().nonnegative('No puede ser negativo').min(10000, 'No contiene la suficiente cantidad de numeros').max(100000)
+    }),
+    params: z.object({
+        id: z.string()
+    })
+})
+
+export const updateEstadoUserSchema = z.object({
+    body: z.object({
+        TotalCaja: z.number().nonnegative('No puede ser negativo').min(50000, 'No contiene la suficiente cantidad de numeros').optional(),
+        TotalTarjeta: z.number().nonnegative('No puede ser negativo').min(1, 'No contiene la suficiente cantidad de numeros').optional(),
+        TotalTransf: z.number().nonnegative('No puede ser negativo').optional(),
+        SiguienteDia: z.number().nonnegative('No puede ser negativo').min(10000, 'No contiene la suficiente cantidad de numeros').max(100000).optional()
+    }),
+    params: z.object({
+        id: z.string()
+    })
+})
+
+export const deleteEstadoUserSchema = z.object({
+    body: z.object({
+        pass: z.number().nonnegative('No puede ser negativo').min(4, 'No contiene la suficiente cantidad de numeros'),
+    }),
+    params: z.object({
+        id: z.string()
+    })
+})
