3e70c5e0405c61557e0663b460c2c3cf4ff3ff3f
Chispa <camilomonterom@gmail.com>
2023-04-20 10:35:05 -0400

    db edited

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import {createPool} from "mysql2/promise"
 export async function connect(){
     const mmelyconnection = await createPool({
         host: 'mmelymysql.cz5fvuemhkod.sa-east-1.rds.amazonaws.com',
-        user: 'Chispitein',
+        user: 'Chispa',
         password: 'Dalealbo7',
         database: 'mmely',
         connectionLimit: 10
