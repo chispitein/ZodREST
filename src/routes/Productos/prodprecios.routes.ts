2733b069c3d48055441178a8d3d141ec79295b61
Chispa <camilomonterom@gmail.com>
2023-06-28 14:19:59 -0400

    asd

--- src/routes/Productos/prodprecios.routes.ts
+++ src/routes/Productos/prodprecios.routes.ts
@@ -5,12 +5,7 @@ import { postHistorySchema, updateHistorySchema, deleteHistorySchema } from "../
 
 const router = Router();
 
-router.get('/prodprecios/search', getFilterTabla('prodprecios'))
 router.get('/prodprecios/', getInnerTablas('productos.idProdCodigo, productos.NombreProd, productos.Cantidad, tipoproducto.TipoProducto,historial.PrecioCompra ,historial.PrecioVenta, historial.PorGanancia, unidadmedidas.Unidad', 'productos inner join historial on historial.idProdCodigo = productos.idProdCodigo inner JOIN tipoproducto on productos.idTipoProducto = tipoproducto.idTipoProducto INNER JOIN unidadmedidas on unidadmedidas.idUnidadMedida = productos.idUnidadMedida;'))
-router.post('/prodprecios', schemaValidation(postHistorySchema) , InsertRow('prodprecios') );
-router.put('/prodprecios/:idprodprecios', schemaValidation(updateHistorySchema) , UpdateRow('prodprecios') );
-router.delete('/prodprecios/:idprodprecios', schemaValidation(deleteHistorySchema) , deleteRow('prodprecios') );
-
 
 
 export default router;
