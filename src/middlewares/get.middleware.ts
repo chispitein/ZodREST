c62a5f2ff998be6efd4bba7d9231af8593437a23
Chispa <camilomonterom@gmail.com>
2023-07-06 16:07:12 -0400

    tsc

--- src/middlewares/get.middleware.ts
+++ src/middlewares/get.middleware.ts
@@ -160,9 +160,8 @@ export const getoneInnerTables = () => async (req: Request, res: Response) => {
     const conn = await connect();
     try {
       const result = await conn.query(
-        "select productos.idProdCodigo, productos.NombreProd ,productos.NombreProd, productos.Cantidad, tipoproducto.TipoProducto,historial.PrecioCompra ,historial.PrecioVenta, historial.PorGanancia, unidadmedidas.Unidad from productos join historial on historial.idProdCodigo = productos.idProdCodigo JOIN tipoproducto on productos.idTipoProducto = tipoproducto.idTipoProducto JOIN unidadmedidas on unidadmedidas.idUnidadMedida = productos.idUnidadMedida WHERE productos.idProdCodigo=" +
-          "?",
-        req.params.idProdCodigo
+        "select productos.idProdCodigo, productos.NombreProd ,productos.NombreProd, productos.Cantidad, tipoproducto.TipoProducto,historial.PrecioCompra ,historial.PrecioVenta, historial.PorGanancia, unidadmedidas.Unidad from productos join historial on historial.idProdCodigo = productos.idProdCodigo JOIN tipoproducto on productos.idTipoProducto = tipoproducto.idTipoProducto JOIN unidadmedidas on unidadmedidas.idUnidadMedida = productos.idUnidadMedida WHERE productos.?",
+        req.params
       );
       return await res.json(result[0]);
     } catch (error) {
