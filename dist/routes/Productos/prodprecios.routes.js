86a6f79fcb9bc36322487d68116f2f87e954fd6b
Chispa <camilomonterom@gmail.com>
2023-07-06 16:08:44 -0400

    asd

--- dist/routes/Productos/prodprecios.routes.js
+++ dist/routes/Productos/prodprecios.routes.js
@@ -4,6 +4,6 @@ const express_1 = require("express");
 const get_middleware_1 = require("../../middlewares/get.middleware");
 const router = (0, express_1.Router)();
 router.get("/prodprecios", (0, get_middleware_1.getInnerTablas)());
-router.get("/prodprecios/:idProdCodigo", (0, get_middleware_1.getoneInnerTables)());
+router.get("/prodprecios/search/:idProdCodigo", (0, get_middleware_1.getoneInnerTables)());
 exports.default = router;
 //# sourceMappingURL=prodprecios.routes.js.map
