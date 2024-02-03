89615636490464b65fdfb43589577779f4b9a203
Chispa <camilomonterom@gmail.com>
2024-02-03 13:09:27 -0300

    tamanostring

--- dist/schemas/Productos/product.schema.js
+++ dist/schemas/Productos/product.schema.js
@@ -4,31 +4,43 @@ exports.deleteProductSchema = exports.updateProductSchema = exports.postProductS
 const zod_1 = require("zod");
 exports.postProductSchema = zod_1.z.object({
     body: zod_1.z.object({
-        NombreProd: zod_1.z.string().nonempty('No puede estar vacio').min(6, 'nombre de menos de 6 caracteres').max(60, "Nombre demasiado largo -60"),
+        NombreProd: zod_1.z
+            .string()
+            .nonempty("No puede estar vacio")
+            .min(6, "nombre de menos de 6 caracteres")
+            .max(60, "Nombre demasiado largo -60"),
         Tamano: zod_1.z.string().nonempty(),
         idTipoProducto: zod_1.z.number().nonnegative(),
-        Cantidad: zod_1.z.number().nonnegative(),
+        Cantidad: zod_1.z.string().nonempty(),
         idUnidadMedida: zod_1.z.number().nonnegative(),
-    })
+    }),
 });
 exports.updateProductSchema = zod_1.z.object({
     body: zod_1.z.object({
-        NombreProd: zod_1.z.string().nonempty('No puede estar vacio').min(6, 'nombre de menos de 6 caracteres').max(60, "Nombre demasiado largo -60").optional(),
+        NombreProd: zod_1.z
+            .string()
+            .nonempty("No puede estar vacio")
+            .min(6, "nombre de menos de 6 caracteres")
+            .max(60, "Nombre demasiado largo -60")
+            .optional(),
         Tamano: zod_1.z.string().nonempty().optional(),
         idTipoProducto: zod_1.z.number().nonnegative().optional(),
         Cantidad: zod_1.z.number().nonnegative().optional(),
         idUnidadMedida: zod_1.z.number().nonnegative().optional(),
     }),
     params: zod_1.z.object({
-        idProdCodigo: zod_1.z.string()
-    })
+        idProdCodigo: zod_1.z.string(),
+    }),
 });
 exports.deleteProductSchema = zod_1.z.object({
     body: zod_1.z.object({
-        pass: zod_1.z.number().nonnegative('No puede ser negativo').min(6, 'No contiene la suficiente cantidad de numeros'),
+        pass: zod_1.z
+            .number()
+            .nonnegative("No puede ser negativo")
+            .min(6, "No contiene la suficiente cantidad de numeros"),
     }),
     params: zod_1.z.object({
-        idProdCodigo: zod_1.z.string()
-    })
+        idProdCodigo: zod_1.z.string(),
+    }),
 });
 //# sourceMappingURL=product.schema.js.map
--- src/schemas/Productos/product.schema.ts
+++ src/schemas/Productos/product.schema.ts
@@ -1,33 +1,45 @@
 import { z } from "zod";
 
 export const postProductSchema = z.object({
-    body: z.object({
-        NombreProd: z.string().nonempty('No puede estar vacio').min(6, 'nombre de menos de 6 caracteres').max(60, "Nombre demasiado largo -60"),
-        Tamano: z.string().nonempty(),
-        idTipoProducto: z.number().nonnegative(),
-        Cantidad: z.number().nonnegative(),
-        idUnidadMedida: z.number().nonnegative(),
-    })
-})
+  body: z.object({
+    NombreProd: z
+      .string()
+      .nonempty("No puede estar vacio")
+      .min(6, "nombre de menos de 6 caracteres")
+      .max(60, "Nombre demasiado largo -60"),
+    Tamano: z.string().nonempty(),
+    idTipoProducto: z.number().nonnegative(),
+    Cantidad: z.string().nonempty(),
+    idUnidadMedida: z.number().nonnegative(),
+  }),
+});
 
 export const updateProductSchema = z.object({
-    body: z.object({
-        NombreProd: z.string().nonempty('No puede estar vacio').min(6, 'nombre de menos de 6 caracteres').max(60, "Nombre demasiado largo -60").optional(),
-        Tamano: z.string().nonempty().optional(),
-        idTipoProducto: z.number().nonnegative().optional(),
-        Cantidad: z.number().nonnegative().optional(),
-        idUnidadMedida: z.number().nonnegative().optional(),
-    }),
-    params: z.object({
-        idProdCodigo: z.string()
-    })
-})
+  body: z.object({
+    NombreProd: z
+      .string()
+      .nonempty("No puede estar vacio")
+      .min(6, "nombre de menos de 6 caracteres")
+      .max(60, "Nombre demasiado largo -60")
+      .optional(),
+    Tamano: z.string().nonempty().optional(),
+    idTipoProducto: z.number().nonnegative().optional(),
+    Cantidad: z.number().nonnegative().optional(),
+    idUnidadMedida: z.number().nonnegative().optional(),
+  }),
+  params: z.object({
+    idProdCodigo: z.string(),
+  }),
+});
 
 export const deleteProductSchema = z.object({
-    body: z.object({
-        pass: z.number().nonnegative('No puede ser negativo').min(6, 'No contiene la suficiente cantidad de numeros'),
-    }),
-    params: z.object({
-        idProdCodigo: z.string()
-    })
-})
+  body: z.object({
+    pass: z
+      .number()
+      .nonnegative("No puede ser negativo")
+      .min(6, "No contiene la suficiente cantidad de numeros"),
+  }),
+  params: z.object({
+    idProdCodigo: z.string(),
+  }),
+});
